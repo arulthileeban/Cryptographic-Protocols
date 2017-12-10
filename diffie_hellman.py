460d2a296dc6b7df8f309dc9df87009a3b20aada
arulthileeban <arulthileeban023@gmail.com>
2017-12-11 00:35:12 +0530

    Added fast modular exponentiation

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -1,2 +1,14 @@
 def discrete_mod(a,b,p):
     return (a**b)%p
+    
+def discrete_mod_fast(a,b,p):
+	if p==1:
+		return 0
+	c=1
+	for i in range(1,b+1):
+		c=(c*a)%p
+	return c
+
+if __name__=="__main__":
+	print discrete_mod(7,5,4)
+	print discrete_mod_fast(7,5,4)
