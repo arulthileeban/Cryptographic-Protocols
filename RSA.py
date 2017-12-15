c9a14fa49a3554b483390f47e573dce3ad01da37
arulthileeban <arulthileeban023@gmail.com>
2017-12-15 23:40:21 +0530

    Added string to integer mapping

--- RSA.py
+++ RSA.py
@@ -1,12 +1,19 @@
 import random
 import timeit
+import getpass
+import string
+values = dict()
+
+for index, letter in enumerate(string.ascii_lowercase):
+   values[letter] = index + 1
+
 def primes(n):
     sieve = [True] * n
     for i in xrange(3,int(n**0.5)+1,2):
         if sieve[i]:
             sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
     return [2] + [i for i in xrange(3,n,2) if sieve[i]]
-   
+
 def gen_pubkey():
 	primes_list=primes(1000)
 	P=random.choice(primes_list)
@@ -24,5 +31,10 @@ def gen_prikey(P,Q,e):
 def get_pair():
 	(P,Q,n,e) = gen_pubkey()
 	d = gen_prikey(P,Q,e)
-	
+
 print(timeit.timeit("get_pair()","from __main__ import get_pair", number = 1))
+
+def encode():
+    data=getpass.getpass()
+    for
+    c = data**e%n
